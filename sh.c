85bf0107a8f420e75a940d42ab3363516a8ddea6
Nathan An <nathanzan0310@gmail.com>
2023-11-06 06:06:28 -0500

    onto ch6

--- sh.c
+++ sh.c
@@ -25,12 +25,18 @@ int main(void) {
     char *argv[512];
     int redirects[512];  // int array of redirect char indexes, excluding file
     // destinations
+    char *background_command[1] = {""}; //background_command string
     int argc;
     int n = 1;
     int stdin_copy = dup(STDIN_FILENO);
     int stdout_copy = dup(STDOUT_FILENO);
     int fileIn;   // new input file
     int fileOut;  // new output file
+    int pid;
+    int backgroundF = 0; // background flag
+    int validCommand = 1;
+    int jid = 1;
+    job_list_t *job_list = init_job_list();
 
     sigset_t old;
     sigset_t full;
@@ -57,17 +63,33 @@ int main(void) {
         int wstatus;
         while ((wret = waitpid(-1, &wstatus, WNOHANG | WUNTRACED)) > 0) {
             if (WIFEXITED(wstatus)) {
+                printf("[%d] (%d) terminated with exit status %d\n", get_job_jid(job_list, wret),
+                       get_job_pid(job_list, wret),
+                       wstatus);
+//                printf("pid: %d\nwret: %d\n",pid, wret);
+                remove_job_pid(job_list, wret);
             }
             if (WIFSIGNALED(wstatus)) {
+                printf("[%d] (%d) terminated with exit status %d\n", get_job_jid(job_list, wret),
+                       get_job_pid(job_list, wret),
+                       wstatus);
+                remove_job_pid(job_list, wret);
             }
             if (WIFSTOPPED(wstatus)) {
+                add_job(job_list, jid, wret, STOPPED, background_command[0]);
             }
         }
 #ifdef PROMPT
-        if (printf("33sh> ") < 0) exit(1);
-        if (fflush(stdout) < 0) exit(1);
+        if (printf("33sh> ") < 0) {
+            cleanup_job_list(job_list);
+            exit(1);
+        }
+        if (fflush(stdout) < 0) {
+            cleanup_job_list(job_list);
+            exit(1);
+        }
 #endif
-        n = (int)read(STDIN_FILENO, buf, sizeof(buf));
+        n = (int) read(STDIN_FILENO, buf, sizeof(buf));
         buf[n] = '\0';
         memset(tokens, '\0', 512 * 4);  // reset tokens, argv, and redirects
         // every time we loop through
@@ -75,10 +97,13 @@ int main(void) {
         memset(redirects, -1,
                512 * 4);  // redirects must be set to -1 due to overlap with
         // possible redirect index values (e.g. 0)
+//        memset(background_command, '\0', 4);
         argc = 0;
+        validCommand = 1;
+        backgroundF = 0;
         parse(buf, tokens, argv, redirects);
-
-        if (redirects[0] != -1) {
+        while (argv[argc] != NULL) argc++;
+        if (redirects[0] != -1) { // checking for redirects
             int i = 0;
             int oc = 0;   // output redirect counter
             int ic = 0;   // input redirect counter
@@ -147,15 +172,20 @@ int main(void) {
                 }
             }
         }
+        if (argc > 0 && strcmp(argv[argc - 1], "&") == 0) { //checking for &
+            backgroundF = 1;
+            argv[argc - 1] = NULL;
+        }
         if (!tokens[0])
             continue;
         else if (strcmp(tokens[0], "exit") == 0) {  // builtin functions: exit
             if (tokens[1] != NULL) {
                 fprintf(stderr, "%s: syntax error\n", tokens[0]);
-            } else
+            } else {
+                cleanup_job_list(job_list);
                 exit(0);
+            }
         } else if (strcmp(tokens[0], "cd") == 0) {  // builtin functions: cd
-            while (argv[argc] != NULL) argc++;
             if (argc != 2) {
                 fprintf(stderr, "%s: syntax error\n", tokens[0]);
                 continue;
@@ -165,6 +195,7 @@ int main(void) {
             DIR *d = opendir(".");
             if (d == NULL) {
                 perror("Error opening directory");
+                cleanup_job_list(job_list);
                 exit(1);
             }
             int found = 0;
@@ -179,6 +210,7 @@ int main(void) {
             if (found) {
                 if (chdir(argv[1]) != 0) {
                     perror("Error changing directory");
+                    cleanup_job_list(job_list);
                     exit(1);
                 }
             } else {
@@ -186,7 +218,6 @@ int main(void) {
                 continue;
             }
         } else if (strcmp(tokens[0], "ln") == 0) {  // builtin functions: ln
-            while (argv[argc] != NULL) argc++;
             if (argc != 3) {
                 fprintf(stderr, "%s: syntax error\n", tokens[0]);
                 continue;
@@ -203,7 +234,6 @@ int main(void) {
                 continue;
             }
         } else if (strcmp(tokens[0], "rm") == 0) {  // builtin functions: rm
-            while (argv[argc] != NULL) argc++;
             if (argc != 2) {
                 fprintf(stderr, "%s: syntax error\n", tokens[0]);
                 continue;
@@ -212,47 +242,66 @@ int main(void) {
                 fprintf(stderr, "%s: No such file or directory.\n", tokens[0]);
                 continue;
             }
+        } else if (strcmp(tokens[0], "jobs") == 0) { //builtin functions: job
+            if (argc != 1) {
+                fprintf(stderr, "%s: syntax error\n", tokens[0]);
+                continue;
+            }
+            jobs(job_list);
         } else {
-            if (!fork()) {
-                int filepath = 0;  // filepath index accounting for redirects
-                for (int i = 0; redirects[i] != -1; i++) {
-                    if (redirects[i] == 0) {
-                        filepath += 2;
-                    }
-                }
+            int filepath = 0;  // filepath index accounting for redirects
+            for (int i = 0; redirects[i] != -1; i++) if (redirects[i] == 0) filepath += 2;
+            background_command[0] = tokens[filepath];
+            pid = fork();
+            if (pid == 0) {
                 int childPid = getpid();
                 int childPGRP = getpgrp();
                 if (setpgid(childPid, 0) < 0) {
                     perror("Child setpgid() error");
+                    cleanup_job_list(job_list);
                     exit(1);
                 }
                 if (tcsetpgrp(STDIN_FILENO, childPGRP) < 0) {
-                    perror("Child tcsetgrp() error");
+                    perror("Child tcsetpgrp() error");
+                    cleanup_job_list(job_list);
                     exit(1);
                 }
                 if (signal(SIGINT, SIG_DFL) == SIG_ERR) {
                     perror("SIGINT child handler error.");
+                    cleanup_job_list(job_list);
                     exit(1);
                 }
                 if (signal(SIGTSTP, SIG_DFL) == SIG_ERR) {
                     perror("SIGTSTP child handler error.");
+                    cleanup_job_list(job_list);
                     exit(1);
                 }
                 if (signal(SIGTTOU, SIG_DFL) == SIG_ERR) {
                     perror("SIGTTOU child handler error.");
+                    cleanup_job_list(job_list);
                     exit(1);
                 }
                 execv(tokens[filepath], argv);
                 perror("execv");
+                validCommand = 0;
+                cleanup_job_list(job_list);
                 exit(1);
             }
-            wait(0);
+            if (backgroundF && validCommand) {
+                printf("[%d] (%d)\n", jid, pid);
+                add_job(job_list, jid, pid, RUNNING, background_command[0]);
+                jid++;
+            }
+            if (!backgroundF) {
+                waitpid(pid, NULL, 0);
+            }
             int shellPid = getpid();
             tcsetpgrp(STDIN_FILENO, shellPid);
             close(fileIn);
             close(fileOut);
             dup2(stdin_copy, STDIN_FILENO);
             dup2(stdout_copy, STDOUT_FILENO);
+
         }
     }
     return 0;
@@ -285,6 +334,7 @@ void sigint_handler(int sig) {
     // printf("Caught signal %d", sig);
     if (signal(sig, SIG_IGN) == SIG_ERR) {
         perror("SIGINT handler error.");
+//        cleanup_job_list(jobs);
         exit(1);
     }
 }
@@ -298,6 +348,7 @@ void sigtstp_handler(int sig) {
     // printf("Caught signal %d", sig);
     if (signal(sig, SIG_IGN) == SIG_ERR) {
         perror("SIGSTP handler error.");
+//        cleanup_job_list(job_list);
         exit(1);
     }
 }
@@ -311,6 +362,7 @@ void sigttou_handler(int sig) {
     //    printf("Caught signal %d", sig);
     if (signal(sig, SIG_IGN) == SIG_ERR) {
         perror("SIGTTOU handler error.");
+//        cleanup_job_list(job_list);
         exit(1);
     }
 }
